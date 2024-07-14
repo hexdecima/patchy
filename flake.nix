19ace3f346c240fe13e505fba2d33d4d2ac847eb
Mai <comfybyte@proton.me>
2024-07-14 18:50:18 -0300

    flake: fix broken build, enable its cache - fix broken downloads during build by `nixpkgs unstable -> nixpkgs   24.06` - enable binary cache for `nixpkgs`

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "comfybyte's experimental Neovim configuration.";
+
   outputs = { nixpkgs, ... }@inputs:
     let
       systems = [ "x86_64-linux" "aarch64-linux" ];
@@ -13,12 +15,14 @@
       packages = eachSystem (pkgs: import ./packages pkgs);
     };
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
   inputs.nightly.url = "github:nix-community/neovim-nightly-overlay/master";
 
   nixConfig = {
-    substituters = [ "https://nix-community.cachix.org" ];
+    substituters =
+      [ "https://cache.nixos.org" "https://nix-community.cachix.org" ];
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
