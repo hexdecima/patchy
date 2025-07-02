f81087d7908a78c1d5ecdc1ad578cb91b1218ef7
Mai <mai@tilde.team>
2025-07-02 19:16:38 -0300

    lua: change theme

--- lua/patchy/init.lua
+++ lua/patchy/init.lua
@@ -1,6 +1,6 @@
 require("patchy.editor")
 require("patchy.plugins")
 
-vim.g.tokyobones_italic_comments = false
-vim.g.tokyobones_transparent_background = true
-vim.cmd.colorscheme("tokyobones")
+vim.g.rosebones_italic_comments = false
+vim.g.rosebones_transparent_background = true
+vim.cmd.colorscheme("rosebones")
