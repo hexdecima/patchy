6f8fb0334892244f593415fe04d29468bb8baf24
Mai <mai@tilde.team>
2025-08-06 23:27:02 -0300

    lua: change colourscheme

--- lua/patchy/init.lua
+++ lua/patchy/init.lua
@@ -1,13 +1,6 @@
 require("patchy.editor")
 require("patchy.plugins")
 
-require("silkcircuit").setup {
-  variant = "soft",
-  styles = {
-    comments = { italic = false },
-    functions = { bold = true, italic = false },
-    strings = { italic = false }
-  }
-}
-
-vim.cmd.colorscheme("silkcircuit")
+vim.g.nordbones_italic_comments = false
+vim.g.nordbones_transparent_background = true
+vim.cmd.colorscheme("nordbones")
